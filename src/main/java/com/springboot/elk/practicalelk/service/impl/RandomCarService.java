d968de12a3fdb27fd1c453a9282e1d9c552608b1
Sang Nguyen <sangndtk@gmail.com>
2023-12-24 21:20:03 +0700

    Modify car service

--- src/main/java/com/springboot/elk/practicalelk/service/impl/RandomCarService.java
+++ src/main/java/com/springboot/elk/practicalelk/service/impl/RandomCarService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.springboot.elk.practicalelk.entity.Car;
 import com.springboot.elk.practicalelk.service.CarService;
+import com.springboot.elk.practicalelk.util.RandomDateUtil;
 
 @Service
 public class RandomCarService implements CarService{
@@ -16,7 +17,18 @@ public class RandomCarService implements CarService{
 	    var color = COLORS.get(ThreadLocalRandom.current().nextInt(COLORS.size()));
 	    var type = TYPES.get(ThreadLocalRandom.current().nextInt(TYPES.size()));
 	    
-	    return new Car(brand, color, type);
+	    var available = ThreadLocalRandom.current().nextBoolean();
+	    var price = ThreadLocalRandom.current().nextInt(5000, 12001);
+	    var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+	    
+	    var car = new Car(brand, color, type);
+	    car.setAvailable(available);
+	    car.setPrice(price);
+	    car.setFirstReleaseDate(firstReleaseDate);
+	    
+//	    return new Car(brand, color, type);
+	    return car;
+	    
 	}
 	
 }
